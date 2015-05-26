ed4d7e0a23d315ed8050bbccbe0f5fac9af20b91
Vladislav Fedotov <fedotov.v.a@gmail.com>
2015-05-26 22:17:49 +0300

    Comments of put and get methods were updated.

--- YandexTeskTask/src/main/java/cache/api/Cache.java
+++ YandexTeskTask/src/main/java/cache/api/Cache.java
@@ -9,7 +9,9 @@ public interface Cache<K, V> {
 
     /**
      * Returns the value associated with the passed key from the cache,
-     * if key does not exist in the cache <b>null</b> will be returned.
+     * if key does not exist in the cache <b>null</b> will be returned.<br />
+     * <b>Pay attention</b>, element that is <tt>put</tt> to the cache will be available for <tt>get</tt> operation
+     * after some timeout that depends current load (number of readers and writes) and the cache <tt>capacity</tt>.
      *
      * @param key key that will be used for value lookup
      * @return value associated withe the passed key or <b>null</b> if key does not exist
@@ -20,7 +22,9 @@ public interface Cache<K, V> {
     /**
      * Puts key-value pair to the cache.<br />
      * Depending of chosen strategy in case cache is full (its size is equal to specified <tt>capacity</tt>)
-     * least recently used or last put node will be removed from the cache.
+     * least recently used or last put node will be removed from the cache.<br />
+     * <b>Pay attention</b>, element that is <tt>put</tt> to the cache will be available for <tt>get</tt> operation
+     * after some timeout that depends current load (number of readers and writes) and the cache <tt>capacity</tt>.
      *
      * @param key   key associated with the value that will be added to the cache, must be <b>not null</b>, otherwise {@link NullPointerException} will be thrown
      * @param value value associated with the key that will be added to the cache, must be <b>not null</b>, otherwise {@link NullPointerException} will be thrown
